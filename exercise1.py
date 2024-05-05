72a251be17a7760358a6d9406a1feb3ce65cc8cf
SRRatul <120864928+SRRatul@users.noreply.github.com>
2024-05-05 06:07:49 +0600

    Exercise 1 | Python Crash Course ( Eric Matthes)
    I am following a book to learn python, here is an exercise.

---
+++ exercise1.py
@@ -0,0 +1,16 @@
+person_name = " Priya "
+msg = "Hello my love" + person_name + "Would you like to learn some Python today?"
+
+print(msg)
+print(person_name.upper())
+print(person_name.lower())
+print(person_name.title())
+
+famous_person = "Albert Einstein "
+quote = (famous_person + "once said, " + '"A person \n who never made a mistake never tried anything new."')
+print(quote)
+new_person = "\t Ratul  "
+print(new_person)
+print(new_person.lstrip())
+print(new_person.rstrip())
+print("Thank you ")
